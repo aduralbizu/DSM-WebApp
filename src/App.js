e225a6e1e133c56c8569ff6123079114039ddd5a
txiriarte <txiriarte@gmail.com>
2024-03-07 13:00:35 +0100

    Ligero cambio y prueba para trabajo colaborativo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, pushing and/or pulling
         </a>
       </header>
     </div>
