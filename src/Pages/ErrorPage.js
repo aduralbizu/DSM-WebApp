b80017b4c12442b08aa8e3a4642db882d8707017
Adur <adur.albizu@gmail.com>
2024-04-09 21:48:07 +0200

    final

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -4,13 +4,17 @@ import './ErrorPage.css'; // Importa el archivo CSS con los estilos
 
 const ErrorPage = () => {
     return (
-        <div className="error-container">
-            <div className="error-content">
-                <h2 className="error-heading">ERROR 404: Página no encontrada</h2>
-                <p className="error-message"> Lo sentimos. La dirección web que has especificado no es una página activa de nuestra web. </p>
-                <p className="error-suggestion">Puedes regresar a la página principal haciendo clic <Link to="/">aquí</Link>.</p>
+        <>
+            <p className='text-center im mb-0 pb-0'>404</p>
+            <div className="error-container">
+
+                <div className="error-content">
+                    <h2 className="error-heading">Página no encontrada</h2>
+                    <p className="error-message"> Lo sentimos. La dirección web que has especificado no es una página activa de nuestra web. </p>
+                    <p className="error-suggestion">Puedes regresar a la página principal haciendo clic <Link to="/">aquí</Link>.</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
